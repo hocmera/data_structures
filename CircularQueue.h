ad22dbed3c27205add1c0023014f971c75a5a9bf
hocine.merabti <hocine.merabti@gmail.com>
2019-02-21 14:37:09 -0500

    First circular queue implementation with example

---
+++ CircularQueue.h
@@ -0,0 +1,63 @@
+
+template <class T>
+class CircularQueue 
+{
+    private:
+    size_t queuesize, avlqueuespace;
+    int rd_ptr, wr_ptr;
+    T* queue;
+
+    public:
+    CircularQueue(size_t queuesize);
+    bool EnQeue (const T data);
+    bool DeQeueu(T &data);
+    size_t AvlQueueSpace();
+    ~CircularQueue();
+};
+
+template <class T>
+CircularQueue<T>::CircularQueue(size_t q_size)
+{
+    queue = new T[q_size];
+	queuesize = q_size;
+	avlqueuespace = q_size;
+    rd_ptr = wr_ptr =-1;
+};
+
+template <class T>
+bool CircularQueue<T>::EnQeue(const T data)
+{
+    if (avlqueuespace!=0){
+		wr_ptr = (wr_ptr+1) % this->queuesize;
+        this->queue[wr_ptr] = data;
+        avlqueuespace-=1;
+        return true;
+    }
+    return false;
+};
+
+template <class T>
+bool CircularQueue<T>::DeQeueu(T &data)
+{
+    if (avlqueuespace!=queuesize){
+        rd_ptr = (rd_ptr+1) % this->queuesize;
+        data = this->queue[rd_ptr];
+        avlqueuespace+=1;
+        return true;
+    }
+    return false;
+};
+
+template <class T>
+size_t CircularQueue<T>::AvlQueueSpace()
+{
+    return this->avlqueuespace;
+}
+
+template <class T>
+CircularQueue<T>::~CircularQueue()
+{
+    delete [] this->queue;
+}
+
+
---
+++ Example.cpp
@@ -0,0 +1,57 @@
+// CircularQueue.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "CircularQueue.h"
+
+using namespace std;
+
+int main()
+{
+
+	 size_t queuesize; // queue size
+	 int data; // data variable used for queue/dequeue
+	 int typed; // user operation selection
+
+	 cout<< "****GENERIC QUEUE IMPLEMENTATION****"<<endl;
+	 cout<< "please enter the queue size: ";
+	 cin>>queuesize;
+	 CircularQueue<int>* queue1 = new CircularQueue<int>(queuesize); // dynamic memory allocation of an int-type queue
+
+
+	 do {
+	     cout<<"what do you want do? enqueue: 1, dequeue: 2, available space: 3. quit: 4"<<endl;
+	     cin>>typed;
+	     switch(typed){
+	         case 1:
+	         cout<< "enter an integer: "<<endl;
+	         cin>>data;
+	         if (queue1->EnQeue(data));
+	         else cout<<"QUEUE IS FULL!"<<endl;
+	         cout<<"available memory is: "<<queue1->AvlQueueSpace()<<endl;
+	         break;
+	         case 2:
+	         if (queue1->DeQeueu(data)){
+	         cout<<data<<endl;
+	         } else cout<<"NOTHING TO READ!"<<endl;
+	         break;
+	         case 3:
+	         cout<<"available memory is: "<<queue1->AvlQueueSpace()<<endl;
+	         break;
+	         case 4:
+	         queue1->~CircularQueue();
+	         cout<<"good bye!"<<endl;
+	         break;
+	     }
+
+	 } while (typed!=4);
+
+
+	return 0;
+
+}
+
+
+
+
